552b093e31cf5eae58a9ae8556deb7b7e99afbb1
Canpaii <cansilay70@gmail.com>
2024-05-09 21:18:06 +0300

    New logic for enemy lock on/off
    Its still not done but locking camera has some logic now and it transitions between cameras, only on the second camera the movement gets bugged so gottta fix that

--- Scrunglys - Staat 1 Periode 4/Assets/Scripts/Can/Camera/CameraLock.cs
+++ Scrunglys - Staat 1 Periode 4/Assets/Scripts/Can/Camera/CameraLock.cs
@@ -3,66 +3,82 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CameraLock : MonoBehaviour
 {
     [Header("References")]
+    public Camera mainCamera;
     public CinemachineFreeLook freeLook;
+    public CinemachineVirtualCamera enemyCam;
     public Transform player;
-    public Transform enemy;
-    public ThirdPersonCam cam;
-    
+    private Transform enemy;
+
     [Header("Bools")]
     public bool locked;
 
-    [Header("Transition")]
-    public float smooth;
-    public bool rotateAgain;
+    [Header("Lock on settings")]
+    public LayerMask enemyLayer;
+    public float radius;
+    public float maxAngle;
 
     public void Update()
     {
         Inputs();   
-        Rotate();
     }
 
     void Inputs()
     {
         if (Input.GetKeyDown(KeyCode.Mouse1) && !locked)
         {
+            enemyCam.LookAt = CalcEnemyLock();
+            enemy = CalcEnemyLock();
             locked = true;
+            SetCamera();
         }
         else if (Input.GetKeyDown(KeyCode.Mouse1)  && locked) 
         {
             locked = false;
+            SetCamera();
         }
     }
-
-    void Transition(Transform target)
+    Transform CalcEnemyLock()
     {
+        Collider[] nearbyEnemy = Physics.OverlapSphere(transform.position, radius, enemyLayer);
+        float closestAngle = maxAngle;
+        Transform closestTarget = null;
 
-        Vector3 startPos = freeLook.transform.position;
-        Quaternion startRot = freeLook.transform.rotation;
+        if(nearbyEnemy.Length <= 0)
+        {
+            return null;
+        }
 
+        for(int i = 0; i < nearbyEnemy.Length; i++)
+        {
+            Vector3 dir = nearbyEnemy[i].transform.position - mainCamera.transform.position;
+            dir.y = 0;
 
-        freeLook.transform.position = Vector3.Lerp(startPos, target.position, smooth);
-        freeLook.transform.rotation = Quaternion.Slerp(startRot, Quaternion.LookRotation(target.position - freeLook.transform.position), smooth);
-
-        freeLook.LookAt = target;
+            float angle = Vector3.Angle(mainCamera.transform.forward, dir);
+            if (angle < maxAngle)
+            {
+                closestTarget = nearbyEnemy[i].transform;
+                closestAngle = angle;
+            }
+        }
+        return closestTarget;
     }
-
-
-    void Rotate()
+    void SetCamera()
     {
         if (locked)
         {
             player.LookAt(enemy);
-            cam.enabled = false;
-            Transition(enemy);
+            freeLook.Priority = 10;
+            enemyCam.Priority = 20;
         }
         else
         {
-            Transition(player);
-            cam.enabled = true;
+            freeLook.Priority = 20;
+            enemyCam.Priority = 10;
         }
     }
 }
